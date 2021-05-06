983605b4c49c2c59c78d67364aaaec77722a6b94
ozcankursun <62161820+ozcankursun@users.noreply.github.com>
2021-05-06 10:41:22 +0300

    aritmetic operations

---
+++ 04_arithmetic_operations.py
@@ -0,0 +1,16 @@
+# Write a Python program to get the the volume of a sphere with radius 8.
+
+pi = 3.1415926535897931
+r= 8.0
+V= 4/3*pi*r*r*r
+print('The volume of the sphere is: ',V)
+
+# Modulo Operator with integers
+
+# 1. Print the modulo 2 of 'x'
+x = 23
+print(23%2)
+
+# 2. Print the modulo 5.5 of 'y'
+y = 12.5
+print(y%5.5)
